ef01972da253d928bf41bb7ba41c13cd72a12cbd
sasunstepanyan777 <sasunstepanyan777@gmail.com>
2019-09-14 23:37:35 +0400

    http provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
-import { Component, ConnectedCallback } from '../core/core';
+import { Component, ConnectedCallback, DisconnectedCallback } from '../core/core';
 import { AppService } from './app.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     template: require('./app.component.html')
 })
-export class AppComponent extends HTMLElement implements ConnectedCallback {
+export class AppComponent extends HTMLElement implements ConnectedCallback, DisconnectedCallback {
+
+    private subscription: Subscription;
 
     constructor(private readonly appService: AppService) {
         super();
     }
 
     public connectedCallback(): void {
-        // this.appService.getData().subscribe(
-        //     (result: any): void => {
-        //         console.log(result.data);
-        //     }
-        // );
+        this.appService.getData().subscribe(
+            (result: any): void => {
+                console.log(result.data);
+            }
+        );
+    }
+
+    public disconnectedCallback(): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
     }
 }
